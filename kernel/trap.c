c8173366ea98070baf2eb3e8733f031025739236
fenghao <fenghao9523@gmail.com>
2021-02-01 00:17:08 +0800

    ready to submit my lab

--- kernel/trap.c
+++ kernel/trap.c
@@ -119,6 +119,10 @@ usertrap(void)
             p->killed = 1;
           }
         }
+      } else {
+        printf("usertrap(): unexpected scause %p pid=%d\n", r_scause(), p->pid);
+        printf("            sepc=%p stval=%p\n", r_sepc(), r_stval());
+        p->killed = 1;
       }
     }
   } else {
